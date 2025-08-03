6ffe52e3ffc99f30c9b9710b1015a6a129aaba40
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-08-03 20:36:52 +0530

    Create 2308-DivideArrayIntoEqualPairs

---
+++ 2308-DivideArrayIntoEqualPairs/2308-DivideArrayIntoEqualPairs.java
@@ -0,0 +1,18 @@
+// Last updated: 8/3/2025, 8:37:35 PM
+class Solution {
+    public boolean divideArray(int[] nums){
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+      
+       
+        
+   
+}
