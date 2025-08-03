ebf6f574c354cbff38a0fcceb6ffa60ad0beb38e
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-08-03 20:36:42 +0530

    Create 2699-CountTheNumberOfFairPairs

---
+++ 2699-CountTheNumberOfFairPairs/2699-CountTheNumberOfFairPairs.java
@@ -0,0 +1,25 @@
+// Last updated: 8/3/2025, 8:37:25 PM
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return count(nums, upper + 1) - count(nums, lower);
+    }
+
+    public static long count(int[] nums, int bound) {
+        int left = 0;
+        int right = nums.length - 1;
+        long ans = 0;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < bound) {
+                ans += (right - left);  // All pairs between left and right are valid
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+}
