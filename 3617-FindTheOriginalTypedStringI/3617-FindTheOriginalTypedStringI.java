3bd689760919ea885c0dc53b89091b4e3088bb32
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-08-03 20:36:22 +0530

    Create 3617-FindTheOriginalTypedStringI

---
+++ 3617-FindTheOriginalTypedStringI/3617-FindTheOriginalTypedStringI.java
@@ -0,0 +1,21 @@
+// Last updated: 8/3/2025, 8:37:05 PM
+class Solution {
+    public int possibleStringCount(String word) {
+        int result = 0;
+        int count = 1;
+
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                count++;
+            } else {
+                result += (count - 1);
+                count = 1;
+            }
+        }
+
+        // Add remaining group
+        result += (count - 1);
+
+        return result + 1;  
+    }
+}
