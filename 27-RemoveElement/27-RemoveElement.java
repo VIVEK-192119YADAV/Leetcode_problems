5b12590d20f59d15070989e0a09e612d4a0f4bf4
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-08-22 21:52:50 +0530

    File updated at 8/22/2025, 9:53:32 PM

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,13 +1,13 @@
-// Last updated: 8/3/2025, 8:39:28 PM
+// Last updated: 8/22/2025, 9:53:32 PM
 class Solution {
     public int removeElement(int[] nums, int val) {
-    int index=0;
-    for(int i=0;i<nums.length;i++){
-        if (nums[i]!=val){
-            nums[index]=nums[i];
-            index++;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
         }
-    }
-    return index;
+        return index;
     }
 }
