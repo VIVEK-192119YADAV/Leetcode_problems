51627ec8dad01ed1100e02b53a7959076949e183
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-08-26 23:04:59 +0530

    Create 2270-NumberofWaystoSplitArray

---
+++ 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.java
@@ -0,0 +1,20 @@
+// Last updated: 8/26/2025, 11:05:41 PM
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+         int count = 0;
+         long arraysum=0;
+         long sum =0;
+         for(int i = 0 ; i<nums.length;i++){
+            arraysum+=nums[i];
+         }
+        for(int i = 0 ; i<nums.length-1;i++){
+            sum+=nums[i];
+            long right = arraysum-sum;
+            
+            if(sum >= right){
+                count++;
+            }
+        }
+        return count;
+    }
+}
