1c6b0d2a02a10db2a985a423e880747417e7a8f1
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-09-02 13:41:47 +0530

    File updated at 9/2/2025, 1:42:29 PM

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,14 +1,29 @@
-// Last updated: 9/2/2025, 1:30:13 PM
+// Last updated: 9/2/2025, 1:42:29 PM
 class Solution {
     public int search(int[] nums, int target) {
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]==target){
-                return j;
-            }
+        // for(int j=0;j<nums.length;j++){
+        //     if(nums[j]==target){
+        //         return j;
+        //     }
              
+        // }
+        // return -1;
+       int l=0;
+       int r=nums.length-1;
+       while(l<=r){
+        int mid =(l+r)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
-       
+        if(nums[mid]<target){
+            l=mid+1;
+        }
+        else {
+            r=mid-1;
+        }
+
+       }
+       return -1;
 
         
     }
