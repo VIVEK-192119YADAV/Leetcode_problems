fd4068a65f12e01590cb8daba3dac7348151088c
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-09-05 11:58:42 +0530

    Create 81-SearchinRotatedSortedArrayII

---
+++ 81-SearchinRotatedSortedArrayII/81-SearchinRotatedSortedArrayII.java
@@ -0,0 +1,39 @@
+// Last updated: 9/5/2025, 11:59:24 AM
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target)return true;
+        
+                if(nums[l]== nums[mid] &&  nums[mid]==nums[r]){
+                    l++;
+                    r--;
+                    continue;
+                    }
+            
+            if(nums[l]<=nums[mid]){
+                if(target>=nums[l] && target <=nums[mid]){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+            else{
+                if(target>nums[mid]&& target<=nums[r]){
+                     l=mid+1;
+
+                }
+                else{
+                    r=mid-1;
+                }
+               
+            }
+        }
+        return  false;
+       
+        
+    }
+}
