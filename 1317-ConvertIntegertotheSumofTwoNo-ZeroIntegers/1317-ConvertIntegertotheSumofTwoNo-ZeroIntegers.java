f6fc3e6e075d3c3fd565c64c648053be37c88192
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-09-08 13:43:01 +0530

    Create 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers

---
+++ 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
@@ -0,0 +1,24 @@
+// Last updated: 9/8/2025, 1:43:42 PM
+
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        int[] ans = new int[2];
+        for (int i = 1; i < n; i++) {
+            int j = n - i;
+            if (!hasZero(i) && !hasZero(j)) { // check whether the numbers consisting of 0 or not.
+                ans[0] = i;
+                ans[1] = j;
+                break;
+            }
+        }
+        return ans;
+    }
+    // number 0 consist kr rha h ya nhi ?
+    private boolean hasZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) return true;
+            num /= 10;
+        }
+        return false;
+    }
+}
