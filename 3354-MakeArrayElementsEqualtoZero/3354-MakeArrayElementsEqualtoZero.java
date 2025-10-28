28c61bfef18b7bef185a72e689a9328d69e7fe0d
VIVEK YADAV <yadavvivek5139@gmail.com>
2025-10-28 12:57:23 +0530

    Create 3354-MakeArrayElementsEqualtoZero

---
+++ 3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.java
@@ -0,0 +1,15 @@
+// Last updated: 10/28/2025, 12:58:09 PM
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int prefix=0, cnt=0, sum=0;
+        for(int x : nums) sum+=x;
+        for(int x : nums){
+            prefix+=x;
+            if (x==0){
+                final int move=Math.max(2-Math.abs(2*prefix-sum), 0);
+                cnt+=move;
+            }
+        }
+        return cnt;
+    }
+}
