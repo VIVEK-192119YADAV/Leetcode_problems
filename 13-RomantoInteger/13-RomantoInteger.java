269dfb30a19d604d155f170f752915c642d86005
VIVEK YADAV <yadavvivek5139@gmail.com>
2026-02-14 00:08:46 +0530

    File updated at 2/14/2026, 12:09:49 AM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,8 +1,33 @@
-// Last updated: 8/27/2025, 7:25:52 PM
-public class Solution {
-    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        int one = (coordinate1.charAt(0) - 'a') + (coordinate1.charAt(1) - '0');
-        int two = (coordinate2.charAt(0) - 'a') + (coordinate2.charAt(1) - '0');
-        return one % 2 == two % 2;
-    }
-}
+// Last updated: 2/14/2026, 12:09:49 AM
+1class Solution {
+2    public int getval(char ch){
+3        switch(ch)
+4        {
+5            case'I':return 1;
+6            case'V':return 5;
+7            case'X':return 10;
+8            case'L':return 50;
+9            case'C':return 100;
+10            case'D':return 500;
+11            case'M':return 1000;
+12            default:return 0;
+13
+14            
+15        }
+16    }
+17    public int romanToInt(String s) {
+18        int n=s.length();
+19        int sum=0;
+20        for(int i=0;i<n;i++){
+21            char ch1=s.charAt(i);
+22            if((i+1)<n && getval(ch1) < getval(s.charAt(i+1)))
+23                sum=sum-getval(ch1);
+24        
+25            else
+26                sum=sum+getval(ch1);
+27            
+28        }
+29        return sum;
+30     
+31    }
+32}
