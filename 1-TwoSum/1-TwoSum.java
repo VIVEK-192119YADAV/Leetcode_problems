6e89aeebeda8732e2713311c4d74833bcf32696a
VIVEK YADAV <yadavvivek5139@gmail.com>
2026-02-15 22:47:51 +0530

    File updated at 2/15/2026, 10:48:53 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,17 +1,17 @@
-// Last updated: 8/3/2025, 8:40:56 PM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
-            }
-        }
-        return new int[]{};
-    }
-}
-      
-      
-        
-    
+// Last updated: 2/15/2026, 10:48:53 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4            for(int j=i+1;j<nums.length;j++){
+5                if(nums[i]+nums[j]==target){
+6                    return new int[]{i,j};
+7                }
+8            }
+9        }
+10        return new int[]{};
+11    }
+12}
+13      
+14      
+15        
+16    
