a0116ac867dbd470fc4a2369862ab5e4f86ee1d5
VIVEK YADAV <yadavvivek5139@gmail.com>
2026-06-16 14:38:55 +0530

    File updated at 6/16/2026, 2:39:46 PM

--- 633-SumofSquareNumbers/633-SumofSquareNumbers.java
+++ 633-SumofSquareNumbers/633-SumofSquareNumbers.java
@@ -1,23 +1,11 @@
-// Last updated: 6/16/2026, 2:33:27 PM
+// Last updated: 6/16/2026, 2:39:46 PM
 1class Solution {
-2    public boolean isPerfectSquare(int num) {
-3        int l=0;
-4        int r=num;
-5     
-6        while(l<=r){
-7            int mid =l+(r-l)/2;
-8            long  sq=(long)mid*(long)mid;
-9        if(sq==num){
-10            return true;}
-11       else if(sq<num){
-12            l=mid +1;
-13        }
-14        else{
-15            r= mid-1;
-16        }
-17
-18        }
-19        return false;
-20        
-21    }
-22}
+2    public int mySqrt(int x) {
+3        int sq= (int)Math.sqrt(x);
+4          
+5    return sq;
+6    }
+7
+8        
+9  
+10}
