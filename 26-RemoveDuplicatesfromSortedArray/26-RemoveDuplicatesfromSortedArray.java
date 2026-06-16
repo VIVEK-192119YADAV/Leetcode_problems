29d2886e7ac6aa9a2af423f5365f8db85b91d178
VIVEK YADAV <yadavvivek5139@gmail.com>
2026-06-16 12:24:49 +0530

    File updated at 6/16/2026, 12:25:39 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,16 +1,27 @@
-// Last updated: 9/2/2025, 12:59:04 PM
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-      int index=0;
-      for(int i=0;i<nums.length;i++){
-        if(nums[i]!=nums[index]){
-            index++;
-            nums[index]=nums[i];
-        }
-      }
-      return index+1;
-    }
-}
+// Last updated: 6/16/2026, 12:25:39 PM
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        int  n= nums.length;
+4        int i=0;
+5        int j=1;
+6        int k=1;
+7        while(j<n){
+8            if(nums[j]==nums[j-1]){
+9                j++;
+10                continue;
+11
+12            }
+13            else{
+14            nums[i+1]=nums[j];
+15            i++;
+16            k++;
+17            }
+18            j++;
+19
+20           
+21        }
+22
+23         return k;
+24        
+25    }
+26}
